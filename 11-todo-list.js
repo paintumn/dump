a359699314b74c6ac1fdedbfa5bf055001021df8
paintumn <154847184+paintumn@users.noreply.github.com>
2023-12-31 20:47:09 -0500

    Add files via upload

---
+++ 11-todo-list.js
@@ -0,0 +1,44 @@
+const todoList = [{
+    name: 'make dinner',
+    dueDate: '2022-12-22'
+ }, {
+    name: 'wash dishes',
+    dueDate: '2022-12-22'
+ }];
+renderTodoList();
+
+function renderTodoList() {
+
+    let todoListHTML = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //sconst dueDate = todoObject.dueDate
+        const { name, dueDate} = todoObject;
+        const html = `
+        <p>
+            ${name} ${dueDate} 
+            <button onclick="
+            todoList.splice(${i}, 1);
+            renderTodoList();
+            ">Delete</button>
+    </p>`;
+        todoListHTML += html;
+    }
+    console.log(todoListHTML);
+
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML;
+}
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+
+    todoList.push(name);
+    console.log(todoList);
+
+    inputElement.value = '';
+
+    renderTodoList();
+}
